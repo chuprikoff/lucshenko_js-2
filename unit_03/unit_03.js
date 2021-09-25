7fed728625b611758cea0d4f7485006e13780c2b
chuprikoff <chuprikoff@yandex.ru>
2021-09-25 19:02:06 +0300

    fixed task 14

--- unit_03/unit_03.js
+++ unit_03/unit_03.js
@@ -185,11 +185,10 @@ document.querySelector(".b-13").onclick = f13;
 // Task 14
 // Дан input .i-141 и .i-142, type=number.  Дан select .s-143, который содержит две операции - +, -, *, / . Дана кнопка b-14, при нажатии на которую срабатывает функция f14. Функция выводит в .out-14 результат операций выбранной в 3-м select к числам введенным в первом и втором input. Например выбрано 1 13 +, нужно вывести результат операции 1+13 т.е.  14.
 
-let i141 = document.querySelector(".i-141").value;
-let i142 = document.querySelector(".i-142").value;
-let s143 = document.querySelector(".s-143").value;
-
 function f14() {
+  let i141 = document.querySelector(".i-141").value;
+  let i142 = document.querySelector(".i-142").value;
+  let s143 = document.querySelector(".s-143").value;
   let result;
   switch (s143) {
     case "+":
