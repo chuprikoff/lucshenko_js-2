99990053362a6f835f38e768a08fd9db304e3b05
chuprikoff <chuprikoff@yandex.ru>
2021-10-27 23:02:19 +0300

    finished weather api

--- 04_homework/script.js
+++ 04_homework/script.js
@@ -18,7 +18,57 @@ function getWeather() {
 }
 
 function showWeather(data) {
+  function temp_units() {
+    let wind_units_arr = document.querySelectorAll("#wind_units");
+    let temp_units_arr = document.querySelectorAll(".temp_unit");
+    for (let i = 0; i < temp_units_arr.length; i++) {
+      switch (units.value) {
+        case "metric":
+          wind_units_arr[i].textContent = " м/с";
+          temp_units_arr[i].innerHTML = " &#8451;";
+          break;
+        case "imperial":
+          temp_units_arr[i].innerHTML = " &#8457;";
+          wind_units_arr[i].textContent = " мил/ч";
+          break;
+        case "standard":
+          wind_units_arr[i].textContent = " м/с";
+          temp_units_arr[i].innerHTML = " &#8490;";
+          break;
+      }
+    }
+  }
+  temp_units();
+  function degToCompass(num) {
+    const val = Math.floor(num / 45 + 0.5);
+    const arr = [
+      "северный",
+      "северо-восточный",
+      "восточный",
+      "юго-восточный",
+      "южный",
+      "юго-западный",
+      "западный",
+      "северо-западный",
+    ];
+    return arr[val % 8];
+  }
   console.log(data);
+  document.querySelector("#name").textContent = data.name;
+  document.querySelector("#temp").textContent = data.main.temp;
+  document.querySelector("#feels_like").textContent = data.main.feels_like;
+  document.querySelector("#description").textContent =
+    data.weather[0]["description"];
+  document.querySelector(
+    ".clouds_icon"
+  ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`;
+  document.querySelector("#humidity").textContent = data.main.humidity;
+  document.querySelector("#pressure").textContent = Math.round(
+    data.main.pressure * 0.75
+  );
+  document.querySelector("#speed").textContent = data.wind.speed;
+  document.querySelector("#deg").textContent = degToCompass(data.wind.deg);
+  document.querySelector("#gust").textContent = data.wind.gust;
 }
 
 document.querySelector(".btn_getWeather").onclick = getWeather;
@@ -31,7 +81,7 @@ window.onload = function () {
     let lon = startPos.coords.longitude;
 
     fetch(
-      `${param.url}weather?lat=${lat}&lon=${lon}&units=metric&APPID=${param.appid}`
+      `${param.url}weather?lat=${lat}&lon=${lon}&units=metric&lang=ru&APPID=${param.appid}`
     )
       .then((weather) => {
         return weather.json();
