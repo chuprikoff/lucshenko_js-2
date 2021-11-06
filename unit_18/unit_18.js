de3548a2adacb72722e020b9e833d1920a394ac7
chuprikoff <chuprikoff@yandex.ru>
2021-11-06 14:30:06 +0300

    finished task6

--- unit_18/unit_18.js
+++ unit_18/unit_18.js
@@ -78,7 +78,21 @@ document.querySelector(".b-4").onclick = t4;
 // Task 5 ============================================
 /*  На странице созданы 3 p.task-5 c атрибутом data. С помощью forEach переберите их и добавьте событие клик. Напишите функцию t5, которая будет запускаться при клике и добавлять атрибут data элемента, по которому кликнули в массив a5_res.*/
 
-function t5() {}
+// document.querySelectorAll(".task-5").forEach((elem) => {
+//   elem.onclick = () => {
+//     a5_res.push(elem.getAttribute("data"));
+//     document.querySelector(".out-5").innerHTML = a5_res;
+//   };
+// });
+
+function t5() {
+  document.querySelectorAll(".task-5").forEach((elem) => {
+    elem.onclick = () => {
+      a5_res.push(elem.getAttribute("data"));
+      document.querySelector(".out-5").innerHTML = a5_res;
+    };
+  });
+}
 
 document.querySelectorAll(".task-5").forEach((elem) => {
   elem.onclick = t5;
