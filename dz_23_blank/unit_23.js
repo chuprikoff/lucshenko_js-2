17046450fd3b65edae26745e695266c8925caa50
chuprikoff <chuprikoff@yandex.ru>
2022-01-02 19:02:16 +0300

    finished unit_23

--- dz_23_blank/unit_23.js
+++ dz_23_blank/unit_23.js
@@ -26,7 +26,6 @@ function t3() {
   let out3 = "";
   a3.forEach((elem, index) => {
     out3 += `${index} ${elem} <br> `;
-    console.log(out2);
   });
   document.querySelector(".out-3").innerHTML = out3;
 }
@@ -37,34 +36,69 @@ document.querySelector(".b-3").onclick = t3;
 // Task 4 ============================================
 /*  Создайте функцию t4 которая записывает  в LS  массив a4 = {hello: world, hi:mahai}. Ключ a4. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-4.*/
 
-function t4() {}
+let a4 = { hello: " world", hi: "mahai" };
+function t4() {
+  localStorage.setItem("a4", JSON.stringify(a4));
+}
+
+document.querySelector(".b-4").onclick = t4;
 
 // ваше событие здесь!!!
 
 // Task 5 ============================================
 /*   При нажатии кнопки b-5 выведите из LS сохранненный массив a4. Выведите в out-5 в формате ключ пробел значение перенос строки. */
 
-function t5() {}
+function t5() {
+  let a5 = JSON.parse(localStorage.getItem("a4"));
+  console.log(a5);
+  let out5 = "";
+  for (key in a5) {
+    out5 += `${key} ${a5[key]}<br>`;
+  }
+  document.querySelector(".out-5").innerHTML = out5;
+}
+
+document.querySelector(".b-5").onclick = t5;
 
 // ваше событие здесь!!!
 
 // Task 6 ============================================
 /*  Создайте функцию t6 которая очищает весь LS. Запуск по кнопке b-6*/
 
-function t6() {}
+function t6() {
+  localStorage.clear();
+}
+
+document.querySelector(".b-6").onclick = t6;
 
 // ваше событие здесь!!!
 
 // Task 7 ============================================
 /*  Создайте input i-7 куда пользователь может вводить числа и строки. Создайте массив a7. Когда пользователь нажимает кнопку b-7 число должно добавляться в массив. Массив должен сохраняться в LS с ключем a7.*/
 
-function t7() {}
+let a7 = [];
+function t7() {
+  let i7 = +document.querySelector(".i-7").value;
+  if (Boolean(i7)) {
+    a7.push(i7);
+  }
+  localStorage.setItem("a7", JSON.stringify(a7));
+  document.querySelector(".i-7").value = "";
+  document.querySelector(".i-7").setAttribute("placeholder", "Enter next info");
+}
+
+document.querySelector(".b-7").onclick = t7;
 
 // ваше событие здесь!!!
 
 // Task 8 ============================================
 /*   Создайте функцию t8 при запуске которой из a7 удаляется последний элемент. После чего массив сохраняется в LS с ключем a7. Использовать массив из предыдущего задания. */
 
-function t8() {}
+function t8() {
+  a7.pop();
+  localStorage.setItem("a7", JSON.stringify(a7));
+}
+
+document.querySelector(".b-8").onclick = t8;
 
 // ваше событие здесь!!!
