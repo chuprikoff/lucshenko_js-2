938d07e949a0a0375c90fb62694230c534706ced
chuprikoff <chuprikoff@yandex.ru>
2022-01-03 20:43:16 +0300

    finished unit_25

--- dz_25_blank/unit_25.js
+++ dz_25_blank/unit_25.js
@@ -1,38 +1,65 @@
+const auth = "auth=7859d9d42a8834141d529577207c9596";
+const address = "http://getpost.itgid.info/index2.php";
+
+function request(method, elem, action, params) {
+  console.log(params);
+  let xhttp = new XMLHttpRequest();
+  let out = "";
+  let elem_out = document.querySelector(elem);
+  xhttp.addEventListener("load", function () {
+    out = this.responseText;
+    elem_out.textContent = out;
+  });
+  xhttp.open(method, `${address}?${auth}&action=${action}${params}`, true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(auth);
+}
 
 // Task 1 ============================================
 /* Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 1. Выведите в out-1 результат. Запускаться функция должна по нажатию b-1. */
 
 function t1() {
+  request("GET", ".out-1", 1, "");
 }
 
+document.querySelector(".b-1").addEventListener("click", t1);
 // ваше событие здесь!!!
 
 // Task 2 ============================================
 /* Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 2. Добавьте параметр name с вашим именем на латинице. Если все сделано верно, сервер пришлет строку hello ваше имя. Выведите в out-2 результат. Запускаться функция должна по нажатию b-2. */
 
 function t2() {
-
+  let name = "Yuriy";
+  request("GET", ".out-2", 2, `&name=${name}`);
 }
 
-// ваше событие здесь!!!
+document.querySelector(".b-2").addEventListener("click", t2);
 
+// ваше событие здесь!!!
 
 // Task 3 ============================================
 /*  Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 3. Добавьте параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет сумму чисел.  Выведите в out-3 результат. Запускаться функция должна по нажатию b-3. */
 
 function t3() {
-
+  let num1 = 2;
+  let num2 = 2;
+  request("GET", ".out-3", 3, `&num1=${num1}&num2=${num2}`);
 }
 
-// ваше событие здесь!!!
+document.querySelector(".b-3").addEventListener("click", t3);
 
+// ваше событие здесь!!!
 
 // Task 4 ============================================
 /*  Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 4. Добавьте параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет случайное число в заданном диапазоне. Не забывайте указывать параметр auth (ключ в чате).  Выведите в out-4 результат. Запускаться функция должна по нажатию b-4. */
 
 function t4() {
-
+  let num1 = 5;
+  let num2 = 2;
+  request("GET", ".out-4", 4, `&num1=${num1}&num2=${num2}`);
 }
+
+document.querySelector(".b-4").addEventListener("click", t4);
 
 // ваше событие здесь!!!
 
@@ -40,8 +67,10 @@ function t4() {
 /*  Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 5. Если все сделано верно, сервер вернет текущее время и дату. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-5 результат. Запускаться функция должна по нажатию b-5. */
 
 function t5() {
-
+  request("GET", ".out-5", 5, "");
 }
+
+document.querySelector(".b-5").addEventListener("click", t5);
 
 // ваше событие здесь!!!
 
@@ -49,18 +78,23 @@ function t5() {
 /*  Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 6. Добавьте параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет большее число. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-6 результат. Запускаться функция должна по нажатию b-6. */
 
 function t6() {
-
+  let num1 = 26;
+  let num2 = 56;
+  request("GET", ".out-6", 6, `&num1=${num1}&num2=${num2}`);
 }
 
-// ваше событие здесь!!!
+document.querySelector(".b-6").addEventListener("click", t6);
 
+// ваше событие здесь!!!
 
 // Task 7 ============================================
 /*  Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 7. Если все сделано верно, сервер случайную ссылку на изображение. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-7 результат. Запускаться функция должна по нажатию b-7. */
 
 function t7() {
-
+  request("GET", ".out-7", 7, "");
 }
+
+document.querySelector(".b-7").addEventListener("click", t7);
 
 // ваше событие здесь!!!
 
@@ -68,21 +102,27 @@ function t7() {
 /* Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 8. В качестве параметра по очереди укажите year равный году вашего рождения. Если все правильно сервер вернет ваш возраст. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-8 результат. Запускаться функция должна по нажатию b-8.*/
 
 function t8() {
-
+  let year = 1974;
+  request("GET", ".out-8", 8, `&year=${year}`);
 }
 
-// ваше событие здесь!!!
+document.querySelector(".b-8").addEventListener("click", t8);
 
+// ваше событие здесь!!!
 
 // Task 9 ============================================
 /* Отправьте GET запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 9. В качестве параметра по очереди укажите m = 1, d=1, y=1. Если все сделано верно, сервер возвратит дату или месяц или год. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-9 результат. Запускаться функция должна по нажатию b-9. */
 
 function t9() {
-
+  let m = 1;
+  let d = 1;
+  let y = 1;
+  request("GET", ".out-9", 9, `&m=${m}&d=${d}&y=${y}`);
 }
 
-// ваше событие здесь!!!
+document.querySelector(".b-9").addEventListener("click", t9);
 
+// ваше событие здесь!!!
 
 // Task 10 ============================================
 /*  Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 1. Если все сделано верно, сервер пришлет строку hello. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-10 результат. Запускаться функция должна по нажатию b-10.
@@ -90,14 +130,19 @@ function t9() {
 */
 
 function t10() {
-
+  request("POST", ".out-10", 1, "");
 }
+document.querySelector(".b-10").addEventListener("click", t10);
+
 // Task 11 ============================================
 /*  Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 2. Добавьте параметр name с вашим именем на латинице. Если все сделано верно, сервер пришлет строку hello ваше имя. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-11 результат. Запускаться функция должна по нажатию b-11. */
 
 function t11() {
-
+  let name = "Yuriy";
+  request("POST", ".out-11", 2, `&name=${name}`);
 }
+
+document.querySelector(".b-11").addEventListener("click", t11);
 
 // ваше событие здесь!!!
 
@@ -105,8 +150,12 @@ function t11() {
 /*  Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 3. Добавьте параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет сумму чисел. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-12 результат. Запускаться функция должна по нажатию b-12.*/
 
 function t12() {
-
+  let num1 = 2;
+  let num2 = 2;
+  request("POST", ".out-12", 3, `&num1=${num1}&num2=${num2}`);
 }
+
+document.querySelector(".b-12").addEventListener("click", t12);
 
 // ваше событие здесь!!!
 
@@ -114,8 +163,12 @@ function t12() {
 /*  Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 4. Добавьте параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет случайное число в заданном диапазоне. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-13 результат. Запускаться функция должна по нажатию b-13.*/
 
 function t13() {
-
+  let num1 = 5;
+  let num2 = 2;
+  request("POST", ".out-13", 4, `&num1=${num1}&num2=${num2}`);
 }
+
+document.querySelector(".b-13").addEventListener("click", t13);
 
 // ваше событие здесь!!!
 
@@ -123,8 +176,10 @@ function t13() {
 /*  Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 5. Если все сделано верно, сервер вернет текущее время и дату. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-14 результат. Запускаться функция должна по нажатию b-14.*/
 
 function t14() {
-
+  request("POST", ".out-14", 5, "");
 }
+
+document.querySelector(".b-14").addEventListener("click", t14);
 
 // ваше событие здесь!!!
 
@@ -132,8 +187,12 @@ function t14() {
 /*  Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 6. Добавьте параметр num1 и num2 содержащие числа. Если все сделано верно, сервер вернет большее число. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-15 результат. Запускаться функция должна по нажатию b-15. */
 
 function t15() {
-
+  let num1 = 26;
+  let num2 = 56;
+  request("POST", ".out-15", 6, `&num1=${num1}&num2=${num2}`);
 }
+
+document.querySelector(".b-15").addEventListener("click", t15);
 
 // ваше событие здесь!!!
 
@@ -141,8 +200,10 @@ function t15() {
 /*  Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 7. Если все сделано верно, сервер случайную ссылку на изображение. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-16 результат. Запускаться функция должна по нажатию b-16. */
 
 function t16() {
-
+  request("POST", ".out-16", 7, "");
 }
+
+document.querySelector(".b-16").addEventListener("click", t16);
 
 // ваше событие здесь!!!
 
@@ -150,8 +211,11 @@ function t16() {
 /*  Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 8. В качестве параметра по очереди укажите year равный году вашего рождения. Если все правильно сервер вернет ваш возраст. Не забывайте указывать параметр auth (ключ в чате).Выведите в out-17 результат. Запускаться функция должна по нажатию b-17. */
 
 function t17() {
-
+  let year = 1974;
+  request("POST", ".out-17", 8, `&year=${year}`);
 }
+
+document.querySelector(".b-17").addEventListener("click", t17);
 
 // ваше событие здесь!!!
 
@@ -159,8 +223,12 @@ function t17() {
 /*  Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 9. В качестве параметра по очереди укажите m = 1, d=1, y=1. Если все сделано верно, сервер возвратит дату или месяц или год. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-18 результат. Запускаться функция должна по нажатию b-18. */
 
 function t18() {
-
+  let m = 1;
+  let d = 1;
+  let y = 1;
+  request("POST", ".out-18", 9, `&m=${m}&d=${d}&y=${y}`);
 }
 
-// ваше событие здесь!!!
+document.querySelector(".b-18").addEventListener("click", t18);
 
+// ваше событие здесь!!!
